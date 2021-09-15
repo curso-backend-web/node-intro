2ea0c2d0e8372e3afadb656150a87b5ab13b3fe7
Roberto Garcia <41040915+naranjito72@users.noreply.github.com>
2021-09-15 10:55:58 +0200

    Finished refac
    Thanks to  my mate I refac the code

--- starter-code/index.js
+++ starter-code/index.js
@@ -4,39 +4,21 @@ class SortedList {
 constructor(){
     this.items = [];
     this.length = 0;
-    this.myHelper = false;
 }
     add(item) {
     this.items.push(item);
-    // this.mySort(this.items);
     this.items.sort((a,b) => a - b);
     this.length++;
 
     }
     get(pos) {
-    // this.helpMe();
       if(this.items.length == 0){
         this.myErr("OutOfBounds Error");
-      } /* else {
-        let arrPos;
-        arrPos = this.items[pos];
-        return arrPos;
-     } */
+      }
      return this.items[pos];
     }
     max() {
-     /*  this.helpMe(); */
-      /*let maxNum = 0;
-      let large = this.items.length;
-      if(this.myHelper == false){
-        this.myErr("EmptyList Error");
-      }  else if(this.items.length == 1){
-        this.items = this.mySort(this.items);
-        maxNum = this.items[0];
-     } else {
-       this.items = this.mySort(this.items);
-       maxNum = this.items.length -1;
-     } */
+    
      if (this.items.length == 0){
       this.myErr("EmptyList Error");
      }
@@ -62,11 +44,7 @@ constructor(){
       }
       return this.items.reduce((a,b) => a + b, 0);
     }
-    // sort
-    // mySort = (arr) => arr.sort((a, b) => a - b);
-    //  helper
-    // helpMe = () => (this.items.length == 0) ? this.myHelper : this.myHelper = true;
-    // error
+    
     myErr = (str) => {
       let err = new Error(str);
       throw err;
