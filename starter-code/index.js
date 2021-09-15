03373f26cbaa8f0be4ac06fc82076eb89b4ec7e5
Your Name <you@example.com>
2021-09-15 12:26:52 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -8,15 +8,81 @@ class SortedList {
 
     add(item) {
 
-        this.items.push(item);
-        this.length++;
+            this.items.push(item);
+            this.length++;
+            this.items.sort(function(a, b){return a - b});
+         
     }
-    get(pos) {}
+    get(pos) {
+        
+         if(this.items[pos-1]!= null){
 
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+                return this.items[pos-1];
+
+         } else {
+
+            throw new TypeError("OutOfBounds Error");
+         }
+        
+    }
+
+    max() {
+
+        if (this.items.length == 0) {
+
+            throw new TypeError('EmptyList Error');
+
+        } else {
+
+            return Math.max.apply(null, this.items);
+        }
+    }
+    min() {
+
+        if (this.items.length == 0) {
+
+            throw new TypeError('EmptyList Error');
+
+        } else {
+
+            return Math.min.apply(null, this.items);
+        }
+    }
+    average() {
+
+        if (this.items.length == 0) {
+
+            throw new TypeError('EmptyList Error');
+
+        } else {
+
+            let total = 0;
+            
+            for (let k=0; k<this.items.length; k++){
+
+                total = this.items[k] + total;
+            }
+            return total/this.items.length;
+        } 
+    }
+    sum() {
+
+        if (this.items.length == 0) {
+
+            throw new TypeError('EmptyList Error');
+
+        } else {
+
+            let total = 0;
+            
+            for (let k=0; k<this.items.length; k++){
+
+                total = this.items[k] + total;
+            }
+            return total;
+        } 
+
+    }
 }
 
 export default SortedList;
